e781701962a5fb12cfd57bfef62037eb3feae5e7
Django Shelton <djangoshelton@gmail.com>
2019-02-19 22:56:33 +0000

    move json files to data folder

--- model_answers/index.js
+++ model_answers/index.js
@@ -1,5 +1,5 @@
-const invoices = require("./invoices.json");
-const plays = require("./plays.json");
+const invoices = require("../data/invoices.json");
+const plays = require("..data/plays.json");
 const statement = require("./1_starting_point");
 
 console.log(statement(invoices[0], plays));
