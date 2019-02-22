4ff95f911f5eca046598e12ffbf47f1996a7980d
Django Shelton <djangoshelton@gmail.com>
2019-02-22 07:30:08 +0000

    first exercise

--- exercises/index.js
+++ exercises/index.js
@@ -1,5 +1,5 @@
 const invoices = require("../data/invoices.json");
 const plays = require("..data/plays.json");
-const statement = require("./1_starting_point");
+const statement = require("./1_starting_point/start");
 
 console.log(statement(invoices[0], plays));
--- model_answers/index.js
+++
@@ -1,5 +0,0 @@
-const invoices = require("../data/invoices.json");
-const plays = require("..data/plays.json");
-const statement = require("./1_starting_point");
-
-console.log(statement(invoices[0], plays));
